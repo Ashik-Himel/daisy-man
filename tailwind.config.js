af792f19f5192805ce8d2b39e5d5b4375951b508
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-07-27 11:13:19 +0600

    nav , hero , collection section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.5rem',
+        md: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem'
+      }
+    }
   },
   plugins: [require('daisyui')],
   daisyui: {
